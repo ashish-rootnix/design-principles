f0b9042d4dd5d5fa7b58b776211596ecabc9c89c
ashish_rootnix <rootnixprogramming@gmail.com>
2026-07-09 00:44:19 +0530

    Started from Lglberger

--- Java/bridge/CombutionCapable.java
+++
@@ -1,9 +0,0 @@
-package bridge;
-
-// To fix LSP violation from main application, this interface is introduced
-public interface CombutionCapable extends Engine
-{
-    // Marker only - only combution engines can implement this
-    void refuel();
-    int getFuelLevel();
-}
--- Java/bridge/ElectricCapable.java
+++
@@ -1,9 +0,0 @@
-package bridge;
-
-// To fix LSP violation from main application, this interface is introduced
-public interface ElectricCapable extends Engine
-{
-    // Marker only - only electric engines can implement this
-    void recharge();
-    int getBatteryLevel();
-}
